b1d8ed8184ae8eaeeeb680305f8abea7ab3df0bd
Fredkr0ket <ferdovbalen@gmail.com>
2022-11-03 22:01:45 +0100

    added stuf to tailwind.config

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,9 @@
 function App() {
 
   return (
-    <></>
+    <div className="max-w-[62.5%] mx-[18.75%]  h-screen ">
+
+    </div>
   )
 }
 
--- app/tailwind.config.cjs
+++ app/tailwind.config.cjs
@@ -5,6 +5,46 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    dropShadow: {
+      '3xl': '0 35px 35px rgba(0, 25, 25, 0.25)',
+      '4xl': [
+          '0 35px 35px rgba(0, 0, 0, 0.25)',
+          '0 45px 65px rgba(0, 0, 0, 0.15)'
+      ]
+    },
+    // margin: {
+    //   '12': '12.5%',
+    //   '25': '25%',
+    //   '33': '33%',
+    //   '50': '50%',
+    //   '62': '62.5%',
+    //   '75': '75%',
+    // },
+    // maxWidth: {
+    //   '12': '12.5%',
+    //   '25': '25%',
+    //   '33': '33%',
+    //   '50': '50%',
+    //   '62': '62.5%',
+    //   '75': '75%',
+    // },
+    colors: {
+      'gray-1': '#262626',
+      'gray-2': '#333333',
+      'gray-3': '#d6d6d6',
+      'white': '#ffffff',
+      'orange': "#ea5a00"
+    },
+    screens: {
+			xl: { max: "1279px" },
+			// => @media (max-width: 1279px) { ... }
+			lg: { max: "1023px" },
+			// => @media (max-width: 1023px) { ... }
+			md: { max: "767px" },
+			// => @media (max-width: 767px) { ... }
+			sm: { max: "639px" },
+			// => @media (max-width: 639px) { ... }
+		},
   },
   plugins: [],
 }
