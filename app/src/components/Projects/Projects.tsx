e72d6459ca5124044885e9df67794c727abecf3d
Fredkr0ket <ferdovbalen@gmail.com>
2022-11-07 19:34:48 +0100

    added images

--- app/src/components/Projects/Projects.tsx
+++ app/src/components/Projects/Projects.tsx
@@ -3,13 +3,15 @@ import Projectitem from "./Projectsitem";
 import projects from '../../data/projects.json'
 
 const Projects = () => {
-  const items = projects.map(item => {
+  const items = projects.projects.map(item => {
     return (
       <Projectitem
       title={item.title}
       description={item.description}
       img={item.img}
       imgAlt={item.imgAlt}
+      tools={item.tools}
+      toolAlt={item.tools} 
       ></Projectitem>
     )
   })
--- app/src/components/Projects/Projectsitem.tsx
+++ app/src/components/Projects/Projectsitem.tsx
@@ -7,22 +7,27 @@ export interface IProjects {
   img?: string | undefined
   imgAlt?: string
   tools?: string[]
+  toolAlt: string[]
 }
 
-const Projectitem = ({title, description, img, imgAlt, tools}: IProjects) => {
+const Projectitem = ({title, description, img, imgAlt, tools, toolAlt}: IProjects) => {
+  console.log(tools)
   return (
     <div className="flex flex-row max-w-[100%] mb-12">
-      <div className="flex flex-col w-[68%] ml-[4%]">
+      <div className="flex flex-col self-end w-[68%] ml-[4%]">
         <h1 className="text-s-title text-orange-2">{title}</h1>
         <p className="text-desc text-gray-3 mb-2">{description}</p>
         <p className="text-ss-title text-orange-2">Tools</p>
-        <div className="flex flex-row self-end max-h-[40px]">
-          <img src="src/assets/Tailwind.svg" className="mr-2"></img>
-          <img src="src/assets/react.svg" className="mr-2"></img>
-          <img src="src/assets/vite.png" className="mr-2"></img>
+        <div className="flex flex-row max-h-[40px] mt-2">
+          {tools?.map(tool => {
+            console.log(tool)
+            return(
+              <img className="mr-4" src={"src/assets/" + tool} alt={tool}></img>
+            )
+          })}
         </div>
       </div>
-        <img className="max-h-[250px]" src={img} alt={imgAlt}></img>
+        <img className="max-h-[250px]" src={"src/assets/" + img} alt={imgAlt}></img>
       
     </div>
   )
