a325df809797b5ba2a02edc18e67ea8750f96edb
Fredkr0ket <ferdovbalen@gmail.com>
2023-06-23 08:40:55 +0200

    checed project button to static

--- src/components/Buttons/ScrollButton.tsx
+++ src/components/Buttons/ScrollButton.tsx
@@ -10,18 +10,12 @@ interface Props {
 }
 
 const ScrollButton: React.FC<Props> = ({target, display}) => {
-    const hoverColor = useColorModeValue('0,0,0,0.2', '255,255,255,0.2')
     const styles = {
         root: css`
             padding: 15px;
             border-radius: 15px;
             text-align: center;
             transition: all 1s ease-in-out;
-            &:hover{
-                margin-top: 20px;
-                // border-radius: 25px;
-                // background-color: rgba(${hoverColor});
-            }
         `,
         text: css`
             font-family: rubiklight;
@@ -36,11 +30,3 @@ const ScrollButton: React.FC<Props> = ({target, display}) => {
 }
 
 export default ScrollButton
-
-const styles ={
-    root: css`
-        padding: 15px;
-        text-align: center;
-    `,
-
-}
