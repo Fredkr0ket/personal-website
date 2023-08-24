c8007a393363f63b61789eceb89bd20c46e87856
Fredkr0ket <ferdovbalen@gmail.com>
2023-08-24 17:54:24 +0200

    made header fade out after scrolling

--- src/components/Buttons/LightmodeSwitch.tsx
+++ src/components/Buttons/LightmodeSwitch.tsx
@@ -1,7 +1,6 @@
 import { useColorMode, useColorModeValue } from "@chakra-ui/react"
-import { css } from "@emotion/css"
-import React from "react"
-
+import { css, keyframes } from "@emotion/css"
+import React, { useEffect, useState } from "react"
 interface Props {
   url: string
 }
@@ -9,6 +8,44 @@ const LightmodeSwitch = () => {
     const { toggleColorMode } = useColorMode()
     const svgState = useColorModeValue('moon', 'sun')
     const hoverColor = useColorModeValue('0,0,0,0.2', '255,255,255,0.2')
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isAtTop, setIsAtTop] = useState(true);
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+
+            if (scrollY > 0) {
+                setIsScrolled(true);
+                setIsAtTop(false);
+            } else {
+                setIsScrolled(false);
+                setIsAtTop(true);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
+    const fadeOutAnimation = keyframes`
+      from {
+          opacity: 1;
+      }
+      to {
+          opacity: 0;
+      }
+    `;
+
+    const fadeInAnimation = keyframes`
+      from {
+          opacity: 0;
+      }
+      to {
+          opacity: 1;
+      }
+    ` ;
     const styles = {
         root: css`
             padding: 4px;
@@ -19,17 +56,23 @@ const LightmodeSwitch = () => {
                 border-radius: 25px;
                 background-color: rgba(${hoverColor});
             }
-        `
+        `,
+        fadeOutText: css`
+            animation: ${fadeOutAnimation} 1s forwards;
+        `,
+        fadeInText: css`
+            animation: ${fadeInAnimation} 1s forwards;
+        `,
     }
     if (svgState == 'moon') {
         return (
-            <button onClick={toggleColorMode} className={styles.root}>
+            <button onClick={toggleColorMode} className={`${styles.root} ${isScrolled && styles.fadeOutText} ${isAtTop && styles.fadeInText}`}>
                 <svg width="38px" height="38px" viewBox="0 0 24 24" fill="black" xmlns="http://www.w3.org/2000/svg"><path d="M12 21.8a9.796 9.796 0 0 1-1.245-19.513l1.286-.163-.843.984a8.285 8.285 0 0 0 8.519 13.383l1.252-.347-.696 1.096A9.755 9.755 0 0 1 12 21.8zM9.647 3.526a8.796 8.796 0 1 0 9.031 14.196 9.048 9.048 0 0 1-1.178.078A9.293 9.293 0 0 1 9.647 3.526z"/><path fill="none" d="M0 0h24v24H0z"/></svg>
             </button>
         )
     } else {
         return(
-            <button onClick={toggleColorMode} className={styles.root}>
+            <button onClick={toggleColorMode} className={`${styles.root} ${isScrolled && styles.fadeOutText} ${isAtTop && styles.fadeInText}`}>
             <svg width="40px" height="40px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                 <path d="M12 16C9.7944 16 8 14.2056 8 12C8 9.7944 9.7944 8 12 8C14.2056 8 16 9.7944 16 12C16 14.2056 14.2056 16 12 16Z" stroke="white"/>
                 <path d="M12 3.5V5.5" stroke="white" stroke-linecap="round"/>
@@ -61,5 +104,13 @@ const styles = {
             background-color: rgba(0,0,0,0.2);
         }
     `
+}
+
+function setIsAtTop(arg0: boolean) {
+    throw new Error("Function not implemented.")
+}
+
+function setIsScrolled(arg0: boolean) {
+    throw new Error("Function not implemented.")
 }
 
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,40 @@
-import { css } from '@emotion/css'
-import React from 'react'
-import GithubButton from './Buttons/GithubButton'
-import LightmodeSwitch from './Buttons/LightmodeSwitch'
-import LinkedinButton from './Buttons/LinkedinButton'
-import '../assets/fonts/major-mono-display-regular/stylesheet.css'
+import { css, keyframes } from '@emotion/css';
+import React, { useState, useEffect } from 'react';
+import GithubButton from './Buttons/GithubButton';
+import LightmodeSwitch from './Buttons/LightmodeSwitch';
+import LinkedinButton from './Buttons/LinkedinButton';
+import '../assets/fonts/major-mono-display-regular/stylesheet.css';
 
 const Header = () => {
+    const [isScrolled, setIsScrolled] = useState(false);
+    const [isAtTop, setIsAtTop] = useState(true);
+
+    useEffect(() => {
+        const handleScroll = () => {
+            const scrollY = window.scrollY;
+
+            if (scrollY > 0) {
+                setIsScrolled(true);
+                setIsAtTop(false);
+            } else {
+                setIsScrolled(false);
+                setIsAtTop(true);
+            }
+        };
+
+        window.addEventListener('scroll', handleScroll);
+
+        return () => {
+            window.removeEventListener('scroll', handleScroll);
+        };
+    }, []);
 
     return (
         <div className={styles.root}>
-            <a href='/'>
-                <h1 className={styles.title}>Ferdo Van Balen</h1>
+            <a href="/">
+                <h1 className={`${styles.title} ${isScrolled && styles.fadeOutText} ${isAtTop && styles.fadeInText}`}>
+                    Ferdo Van Balen
+                </h1>
             </a>
             <div className={styles.iconBox}>
                 <LinkedinButton url={'https://www.linkedin.com/in/ferdo-van-balen-441879241/'} />
@@ -18,8 +42,26 @@ const Header = () => {
                 <LightmodeSwitch />
             </div>
         </div>
-    )
-}
+    );
+};
+
+const fadeOutAnimation = keyframes`
+    from {
+        opacity: 1;
+    }
+    to {
+        opacity: 0;
+    }
+`;
+
+const fadeInAnimation = keyframes`
+    from {
+        opacity: 0;
+    }
+    to {
+        opacity: 1;
+    }
+`;
 
 const styles = {
     root: css`
@@ -31,24 +73,31 @@ const styles = {
         justify-content: space-between;
         align-items: center;
         width: 100%;
-        height:100px;
+        height: 100px;
         background-color: transparent;
         z-index: 1;
         @media (max-width: 718px) {
-            flex-direction:column;
+            flex-direction: column;
             margin-top: 20px;
+            text-align: center;
         }
+    `,
+    fadeOutText: css`
+        animation: ${fadeOutAnimation} 1s forwards;
+    `,
+    fadeInText: css`
+        animation: ${fadeInAnimation} 1s forwards;
     `,
     iconBox: css`
         display: flex;
         align-items: center;
         justify-content: space-around;
-        width:170px;
+        width: 170px;
     `,
     title: css`
         font-family: major_mono_displayregular;
         font-size: 30px;
-    `
-}
+    `,
+};
 
-export default Header
+export default Header;
